edeb936c63c74038608780fada126021c99a78cf
Gaurav Salunke <gauravsalunke252@gmail.com>
2025-10-15 01:43:55 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from 'react-hot-toast';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000/api", // adjust this if your backend runs elsewhere
+    baseURL: "https://steel-logistics-pro.onrender.com", // adjust this if your backend runs elsewhere
     headers: {
         "Content-Type": "application/json",
     },
